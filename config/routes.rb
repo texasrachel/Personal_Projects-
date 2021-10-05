9d5d1bb346cb880f06007ab90f40841febf275b0
Rachel <54075873+texasrachel@users.noreply.github.com>
2021-10-05 14:03:24 -0500

    authentication added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: :create
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
   resources :items
   resources :projects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
