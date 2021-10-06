9c9ea7be674b95ed17bd3510c507c8952846c1b0
Rachel <54075873+texasrachel@users.noreply.github.com>
2021-10-06 14:05:29 -0500

    components and screens installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
+
 
 function App() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  })
+  
+  const [test, setTest] = useState('starting')
+
+  useEffect(() => {
+    setTest
+    }
+  }, [])
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormData({...formData, [name]: value})
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Project Shopping Planner</h1>
+      <form>
+        <input name='username' onChange = {handleChange} />
+        <input name='password' onChange = {handleChange} />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
