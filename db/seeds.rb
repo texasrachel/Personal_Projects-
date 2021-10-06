2680538d9fe9e5f346e867ad164d48b035873534
Rachel <54075873+texasrachel@users.noreply.github.com>
2021-10-06 13:29:32 -0500

    react installed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,27 +10,81 @@ Item.destroy_all
 Project.destroy_all
 User.destroy_all
 
-@admin = User.create!(username: 'chickadee', email: 'chickadee@gmail.com', password: '111111')
+@admin = User.create!(
+  username: 'chickadee', 
+  email: 'chickadee@gmail.com', 
+  password: '111111')
 
 p "Created #{User.count} users"
 
-Project.create!(name: "Backyard",  user: @admin, description: "Add firepit, chairs, and sunsail to backyard", category: "Home")
-Project.create!(name: "Knit Gnome",  user: @admin, description: "Knit a gnome", category: "Knitting")
-Project.create!(name: "Keyboard",  user: @admin, description: "Make a mechanical keyboard", category: "Computers")
-Project.create!(name: "Macarons",  user: @admin, description: "Make macarons", category: "Cooking")
+Project.create!(
+  name: "Backyard",  
+  user: @admin, 
+  description: "Add firepit, chairs, and sunsail to backyard", 
+  category: "Home")
+Project.create!(
+  name: "Knit Gnome",  
+  user: @admin, d
+  escription: "Knit a gnome", 
+  category: "Knitting")
+Project.create!(
+  name: "Keyboard",  
+  user: @admin, 
+  description: "Make a mechanical keyboard", 
+  category: "Computers")
+Project.create!(
+  name: "Macarons",  
+  user: @admin, 
+  description: "Make macarons", 
+  category: "Cooking")
 
 @projects = Project.all
-@projects_limit = Project.where(project_id: project)
+# @projects_limit = Project.where(project_id: project)
 
 p "Created #{Project.count} projects"
 
-Item.create!(name: "Adirondack Chair", projects: @projects_limit, description: "wooden outside chair", category: "furniture", bought: false)
-Item.create!(name: "Firepit", projects: @projects, description: "fire pit", category: "furniture", bought: false)
-Item.create!(name: "Ground Rock", projects: @projects, description: "rock for around firepit", category: "gardening", bought: true)
-Item.create!(name: "Sunsail", projects: @projects, description: "sun shade fabric", category: "gardening", bought: false)
-Item.create!(name: "Sunsail Hook", projects: @projects, description: "hook for house", category: "hardware", bought: false)
-Item.create!(name: "Sunsail Buckle", projects: @projects, description: "sunsail attachments", category: "hardware", bought: false)
-Item.create!(name: "Landscape Fabric", projects: @projects, description: "weed block", category: "gardening", bought: true)
+Item.create!(
+  name: "Adirondack Chair", 
+  projects: @projects, 
+  description: "wooden outside chair", 
+  category: "furniture", 
+  bought: false)
+Item.create!(
+  name: "Firepit", 
+  projects: @projects, 
+  description: "fire pit", 
+  category: "furniture", 
+  bought: false)
+Item.create!(
+  name: "Ground Rock", 
+  projects: @projects, 
+  description: "rock for around firepit", 
+  category: "gardening", 
+  bought: true)
+Item.create!(
+  name: "Sunsail", 
+  projects: @projects, 
+  description: "sun shade fabric", 
+  category: "gardening", 
+  bought: false)
+Item.create!(
+  name: "Sunsail Hook", 
+  projects: @projects, 
+  description: "hook for house", 
+  category: "hardware", 
+  bought: false)
+Item.create!(
+  name: "Sunsail Buckle", 
+  projects: @projects, 
+  description: "sunsail attachments", 
+  category: "hardware", 
+  bought: false)
+Item.create!(
+  name: "Landscape Fabric", 
+  projects: @projects, 
+  description: "weed block", 
+  category: "gardening", 
+  bought: true)
 
 p "Created #{Item.count} items"
 
